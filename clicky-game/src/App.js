99c8853100a53e0d96223916c1bb7c1b9020ee97
jon Opacich <jonopacich@gmail.com>
2018-08-13 21:08:05 -0500

    final

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,47 +1,52 @@
 import React from "react";
-import FriendCard from "./components/FriendCard";
+import Card from "./components/Card";
 import Wrapper from "./components/Wrapper";
-import friends from "./friends.json";
+import cards from "./cards.json";
 import "./App.css";
 
-const removeFriend = () => {
-
-}
+let score=0;
+let highscore=0;
 
 class App extends React.Component {
- state = {
-   friends : friends
- }
- removeFriend = (friendId) => {
-   console.log("removing friend ", friendId);
+  state = {
+    cards: cards,
+    score: score,
+    highscore: highscore
+  };
 
-   const newFriendsList = this.state.friends.filter(
-     (friend) => {
-       if (friend.id === friendId) return false;
-       else return true;
-     }
-   )
-   console.log(newFriendsList);
-   this.setState({friends:newFriendsList});
- }
-render = () => (
- <Wrapper>
-   <h1 className="title">Friends List</h1>
-   {
-     this.state.friends.map(friend => {
-       return <FriendCard
-         removeFriend={this.removeFriend}
-         name={friend.name}
-         image={friend.image}
-         occupation={(friend.occupation)}
-         location={(friend.location)}
-         key={friend.id}
-         id={friend.id}
-       />
-     })
-   }
- </Wrapper>
-);
+  handleScore = () => {
+    this.setState({score:this.state.score+1})
+  };
+
+  removeCard = cardId => {
+    console.log("removing card ", cardId);
+
+    const newCardsList = this.state.cards.filter(card => {
+      if (card.id === cardId) return false;
+      else return true;
+    });
+    console.log(newCardsList);
+    this.setState({ cards: newCardsList });
+  };
+  render = () => (
+    <Wrapper>
+      <h1 className="title">
+        <img
+          src="https://fontmeme.com/permalink/180813/2e4c4ef233b736d7b0a82e13ae50d829.png"
+          alt="sesame-street-font"
+          border="0"
+        />
+        <span class="margin-left">
+          Current Score:
+          {this.state.score} High Score:
+          {this.state.highscore}
+        </span>
+      </h1>
+      {this.state.cards.map(card => {
+        return <a onClick={this.handleScore}><Card image={card.image} /></a>;
+      })}
+    </Wrapper>
+  );
 }
 
 export default App;
